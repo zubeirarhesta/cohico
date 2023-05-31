3c558f14ec2940882a3103529a76de4699001491
zubeirarhesta <zubeirarhesta@gmail.com>
2023-05-31 23:24:50 +0700

    updating contents

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function HomePage(props) {
   return (
     <Fragment>
       <Head>
-        <title>cohico</title>
+        <title>zmarhesta</title>
         <meta name="description" content="code, hike, cook with me" />
       </Head>
       <Hero />
